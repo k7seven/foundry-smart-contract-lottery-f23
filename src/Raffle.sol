2de973fd1e339fb58ec8e85f0468fb728e3b01b2
k7seven <kdoubleseven7@gmail.com>
2023-07-10 17:57:42 -0300

    working with block timestamp

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -30,18 +30,25 @@ pragma solidity ^0.8.18;
  * @dev Implements Chainlink VRFv2
  */
 contract Raffle {
+    /** Errors */
     error Raffle__NotEnoughEthSent();
+    error Raffle__NotEnoughTimeHasPassed();
 
     /** State Variables */
     uint256 private immutable i_entranceFee;
+    /** @dev duration of the raffle in seconds */
+    uint256 private immutable i_interval;
+    uint256 private s_lastTimeStamp;
     address payable[] private s_players;
 
     /** Events */
     event EnteredRaffle(address indexed player);
 
     /** Functions */
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -51,7 +58,10 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+    function pickWinner() external {
+        if ((block.timestamp - s_lastTimeStamp) < i_interval)
+            revert Raffle__NotEnoughTimeHasPassed();
+    }
 
     /** Getter Functions */
     function getEntranceFee() external view returns (uint256) {
