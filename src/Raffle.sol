8f1b762be51eb6e2d6e03daf1c253297779e85f2
k7seven <kdoubleseven7@gmail.com>
2023-07-11 16:04:39 -0300

    getting started with VRFCoordinatorV2Interface

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -23,6 +23,8 @@
 
 pragma solidity ^0.8.18;
 
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
 /**
  * @title A sample Raffle Contract
  * @author Kaue Pacheco
@@ -35,9 +37,19 @@ contract Raffle {
     error Raffle__NotEnoughTimeHasPassed();
 
     /** State Variables */
+    // Constants
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    // Immutables
     uint256 private immutable i_entranceFee;
-    /** @dev duration of the raffle in seconds */
     uint256 private immutable i_interval;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+
+    // Mutables
     uint256 private s_lastTimeStamp;
     address payable[] private s_players;
 
@@ -45,10 +57,21 @@ contract Raffle {
     event EnteredRaffle(address indexed player);
 
     /** Functions */
-    constructor(uint256 entranceFee, uint256 interval) {
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        VRFCoordinatorV2Interface vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) {
         i_entranceFee = entranceFee;
         i_interval = interval;
         s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function enterRaffle() external payable {
@@ -61,6 +84,14 @@ contract Raffle {
     function pickWinner() external {
         if ((block.timestamp - s_lastTimeStamp) < i_interval)
             revert Raffle__NotEnoughTimeHasPassed();
+        /** Getting Random Number */
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
     }
 
     /** Getter Functions */
